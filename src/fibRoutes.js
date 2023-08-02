3b114cbf0885c23bd7ecb90dbd798f90d0931e77
Winston Feng <winstonfeng@HO201903.local>
2023-08-01 21:52:14 -0700

    Debugged and fixed error for Fibonacci Utility to properly return the expected fib output in json

--- src/fibRoutes.js
+++ src/fibRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getFirstNValues } = require("./databaseUtils");
+const { fibonacciSequence } = require("./fibUtility");
 
 //Single post route is needed to post a desired fibonacci N, and then work with
 //db utility to create more fibs if any are needed.
@@ -10,18 +11,31 @@ router.post("/", async (req, res) => {
   try {
     // Get the value of 'n' from the request body
     const { n } = req.body;
+    const parsedN = parseInt(n, 10);
 
     // Get the first 'n' values from the "fibonacci_sequence" table
     const firstNValues = await getFirstNValues("fibonacci_sequence", parsedN);
+
     console.log("endpoint");
     let numberArray = firstNValues.map((item) => item.number);
     console.log(numberArray);
     // Send the first 'n' values as a response
-    res.json(firstNValues);
+    if (firstNValues.length < n) {
+      const additionalFibs = n - firstNValues.length;
+      console.log("additions Fibs needed: ", additionalFibs);
+      console.log(firstNValues);
+      const addedFibs = fibonacciSequence(firstNValues, additionalFibs);
+      console.log(addedFibs);
+      const result = [...firstNValues, ...addedFibs];
+      res.json(result);
+      //   res.json([...firstNValues, ...addedFibs]);
+    } else res.json(firstNValues);
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+//curl -X POST -H "Content-Type: application/json" -d '{"n": 10}' http://localhost:3001/api/fibonacci
 
 module.exports = router;
--- src/fibUtility.js
+++ src/fibUtility.js
@@ -1,21 +1,24 @@
 // Function that takes an array of fibonacci objects and returns n more.
 function fibonacciSequence(arrayData, n) {
   const result = [...arrayData];
+  const final = [];
 
-  // Perform Fibonacci sequence 'n' times
-  for (let i = 2; i < n; i++) {
+  // Perform Fibonacci sequence 'n' times, push to final array
+  for (let i = arrayData.length; i < arrayData.length + n; i++) {
     const nextNumber = result[i - 1].number + result[i - 2].number;
     result.push({ id: i + 1, number: nextNumber });
+    final.push({ id: i + 1, number: nextNumber });
   }
 
-  return result;
+  return final;
 }
 
-const arrayData = [
-  { id: 1, number: 0 },
-  { id: 2, number: 1 },
-];
+//Testing Area
+// const arrayData = [
+//   { id: 1, number: 0 },
+//   { id: 2, number: 1 },
+// ];
 
-console.log(fibonacciSequence(arrayData, 20));
+// console.log(fibonacciSequence(arrayData, 5));
 
 module.exports = { fibonacciSequence };
